19a2e5141437d0620d74ed46d3544cebcf32f6b6
Aschraf-fx <aschrafedzwan@gmail.com>
2026-09-12 14:41:13 +0800

    Load admin event module before portal init

--- app-init.js
+++ app-init.js
@@ -1,2 +1,7 @@
-
-init();
+(function(){
+  const s=document.createElement('script');
+  s.src='app-admin-events.js?v=20260912-1437';
+  s.onload=()=>init();
+  s.onerror=()=>{ console.error('Gagal memuatkan modul Pengurusan Event.'); init(); };
+  document.head.appendChild(s);
+})();
